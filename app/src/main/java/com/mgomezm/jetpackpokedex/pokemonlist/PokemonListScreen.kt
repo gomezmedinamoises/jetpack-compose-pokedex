5ab512c22cba3ed6c85de80fd50b0de85bbdd703
Moises Gomez Medina <moises.gomez@salemwebnetwork.com>
2022-04-20 18:10:05 -0600

    Pagination in Pokemon List Fix bugs related with ImageCoil (deprecated)

--- app/src/main/java/com/mgomezm/jetpackpokedex/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/mgomezm/jetpackpokedex/pokemonlist/PokemonListScreen.kt
@@ -4,13 +4,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
@@ -19,12 +17,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -32,11 +28,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import coil.request.ImageRequest
-import com.google.accompanist.coil.CoilImage
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import com.mgomezm.jetpackpokedex.R
 import com.mgomezm.jetpackpokedex.data.models.PokedexListEntry
 import com.mgomezm.jetpackpokedex.ui.theme.RobotoCondensed
+import kotlinx.coroutines.launch
+
 
 @Composable
 fun PokemonListScreen(
@@ -63,6 +63,8 @@ fun PokemonListScreen(
             ) {
 
             }
+            Spacer(modifier = Modifier.height(16.dp))
+            PokemonList(navController = navController)
         }
     }
 }
@@ -76,7 +78,6 @@ fun SearchBar(
     var text by remember {
         mutableStateOf("")
     }
-
     var isHintDisplayed by remember {
         mutableStateOf(hint != "")
     }
@@ -100,7 +101,6 @@ fun SearchBar(
                     isHintDisplayed = it.isFocused != true
                 }
         )
-
         if (isHintDisplayed) {
             Text(
                 text = hint,
@@ -112,6 +112,46 @@ fun SearchBar(
     }
 }
 
+@Composable
+fun PokemonList(
+    navController: NavController,
+    viewModel: PokemonListViewModel = hiltViewModel()
+) {
+    val pokemonList by remember { viewModel.pokemonList }
+    val endReached by remember { viewModel.endReached }
+    val loadError by remember { viewModel.loadError }
+    val isLoading by remember { viewModel.isLoading }
+
+    LazyColumn(contentPadding = PaddingValues(16.dp)) {
+        val itemCount = if (pokemonList.size % 2 == 0) {
+            pokemonList.size / 2
+        } else {
+            pokemonList.size / 2 + 1
+        }
+        items(itemCount) {
+            if (it >= itemCount - 1 && !endReached) {
+                viewModel.loadPokemonPaginated()
+            }
+            PokedexRow(rowIndex = it, entries = pokemonList, navController = navController)
+        }
+    }
+
+    Box(
+        contentAlignment = Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (isLoading) {
+            CircularProgressIndicator(color = MaterialTheme.colors.primary)
+        }
+        if (loadError.isNotEmpty()) {
+            RetrySection(error = loadError) {
+                viewModel.loadPokemonPaginated()
+            }
+        }
+    }
+}
+
+@ExperimentalCoilApi
 @Composable
 fun PokedexEntry(
     entry: PokedexListEntry,
@@ -144,28 +184,39 @@ fun PokedexEntry(
                 )
             }
     ) {
-        Column() {
-            CoilImage(
-                request = ImageRequest.Builder(LocalContext.current)
-                    .data(entry.imageUrl)
-                    .target {
-                        viewModel.calcDominantColor(it) { color ->
-                            dominantColor = color
-                        }
-                    }
-                    .build(),
+        Column {
+            val painter = rememberImagePainter(
+                data = entry.imageUrl
+            )
+            val painterState = painter.state
+
+            Image(
+                painter = painter,
                 contentDescription = entry.pokemonName,
-                fadeIn = true,
                 modifier = Modifier
                     .size(120.dp)
-                    .align(CenterHorizontally)
-            ) {
+                    .align(CenterHorizontally),
+            )
+            if (painterState is ImagePainter.State.Loading) {
                 CircularProgressIndicator(
                     color = MaterialTheme.colors.primary,
-                    modifier = Modifier.scale(0.5f)
+                    modifier = Modifier
+                        .scale(0.5f)
+                        .align(CenterHorizontally)
                 )
             }
-
+            else if (painterState is ImagePainter.State.Success) {
+                LaunchedEffect(key1 = painter) {
+                    launch {
+                        val image = painter.imageLoader.execute(painter.request).drawable
+                        if (image != null) {
+                            viewModel.calcDominantColor(image) { color ->
+                                dominantColor = color
+                            }
+                        }
+                    }
+                }
+            }
             Text(
                 text = entry.pokemonName,
                 fontFamily = RobotoCondensed,
@@ -176,6 +227,7 @@ fun PokedexEntry(
         }
     }
 }
+
 
 @Composable
 fun PokedexRow(
@@ -190,8 +242,8 @@ fun PokedexRow(
                 navController = navController,
                 modifier = Modifier.weight(1f)
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            if (entries.size >= rowIndex * 2 + 2) {
+            Spacer(modifier = Modifier.width(16.dp))
+            if(entries.size >= rowIndex * 2 + 2) {
                 PokedexEntry(
                     entry = entries[rowIndex * 2 + 1],
                     navController = navController,
@@ -202,5 +254,22 @@ fun PokedexRow(
             }
         }
         Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Composable
+fun RetrySection(
+    error: String,
+    onRetry: () -> Unit
+) {
+    Column {
+        Text(error, color = Color.Red, fontSize = 18.sp)
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = { onRetry() },
+            modifier = Modifier.align(CenterHorizontally)
+        ) {
+            Text(text = "Retry")
+        }
     }
 }
--- app/src/main/java/com/mgomezm/jetpackpokedex/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/com/mgomezm/jetpackpokedex/pokemonlist/PokemonListViewModel.kt
@@ -6,10 +6,15 @@ import android.graphics.drawable.Drawable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.palette.graphics.Palette
 import com.mgomezm.jetpackpokedex.data.models.PokedexListEntry
 import com.mgomezm.jetpackpokedex.repository.PokemonRepository
+import com.mgomezm.jetpackpokedex.util.Constants.PAGE_SIZE
+import com.mgomezm.jetpackpokedex.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -24,10 +29,44 @@ class PokemonListViewModel @Inject constructor(
     var isLoading = mutableStateOf(false)
     var endReached = mutableStateOf(false)
 
-    fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
-        val bitmap = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
+    init {
+        loadPokemonPaginated()
+    }
 
-        Palette.from(bitmap).generate { palette ->
+    fun loadPokemonPaginated() {
+        viewModelScope.launch {
+            isLoading.value = true
+            val result = repository.getPokemonList(PAGE_SIZE, currentPage * PAGE_SIZE)
+            when (result) {
+                is Resource.Success -> {
+                    endReached.value = currentPage * PAGE_SIZE >= result.data!!.count
+                    val pokedexEntries = result.data.results.mapIndexed { index, entry ->
+                        val number = if (entry.url.endsWith("/")) {
+                            entry.url.dropLast(1).takeLastWhile { it.isDigit() }
+                        } else {
+                            entry.url.takeLastWhile { it.isDigit() }
+                        }
+                        val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png"
+                        PokedexListEntry(entry.name.capitalize(Locale.ROOT), url, number.toInt())
+                    }
+                    currentPage++
+
+                    loadError.value = ""
+                    isLoading.value = false
+                    pokemonList.value += pokedexEntries
+                }
+                is Resource.Error -> {
+                    // loadError.value = result.message!!
+                    isLoading.value = false
+                }
+            }
+        }
+    }
+
+    fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
+        val bmp = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+        Palette.from(bmp).generate { palette ->
             palette?.dominantSwatch?.rgb?.let { colorValue ->
                 onFinish(Color(colorValue))
             }
